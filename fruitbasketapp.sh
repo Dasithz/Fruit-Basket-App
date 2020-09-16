31999b2895e3cd58775531894ba81ea97caf1245
Dasithz <dasith9b@gmail.com>
2020-09-15 22:03:55 -0400

    Test Cases sheet with supportive materials.

--- fruitbasketapp.sh
+++ fruitbasketapp.sh
@@ -5,12 +5,18 @@ processedfile=processed.csv
 header=1
 absolutefile=$filepath$filename
 
+if [ ${filename: -4} == ".csv" ];then
+   echo ""
+else
+   echo -e "\e[31m Exit code: 7 $filename not recognized as a CSV file\e[0m" 
+   exit 7
+fi
 
 if [ -f "$filepath$filename" ]; then
     echo -e "\e[33m $filename exists. Continuing to process data..."
     echo -e "\e[0m"
 else
-    echo -e "\e[31m Exit Code: 1 File $filename does not exist in the $filepath. Terminating the script! "
+    echo -e "\e[31m Exit Code: 1 File $filename does not exist in the give path $filepath or no path defined. Terminating the script! "
     echo -e "\e[0m"
     exit 1 
 fi
@@ -75,17 +81,17 @@ echo -e "Total types of \e[31mfruits:\e[0m"
 echo $(($distinct-$header)); echo
 
 echo -e "Oldest fruit & \e[31mage:\e[0m"
-cat $processedfile |awk -v maxVal=$(cat $processedfile |awk -F',' '{print $2}'|sort -n|tail -1) -F',' '{if($2==maxVal) print "\033[0;31m"$1"\033[0m"   ": "$2}'
+cat $processedfile |awk -v maxVal=$(cat $processedfile |awk -F',' '{print tolower($2)}'|sort -n|tail -1) -F',' '{if($2==maxVal) print "\033[0;31m"tolower($1)"\033[0m"   ": "$2}'
 echo
 
 
 echo -e "The number of each type of fruit in descending \e[31morder:\e[0m"
 
-cat $processedfile |awk -F, '{A[$1]++}END{for(i in A)print "\033[0;31m"i"\033[0m" ": " A[i]}'
+cat $processedfile |awk -F, '{A[tolower($1)]++}END{for(i in A)print "\033[0;31m"i"\033[0m" ": " A[i]}'
 echo
 
 echo -e "The various characteristics (count, color, shape, etc.) of each fruit by \e[31mtype:\e[0m"
-cat $processedfile |awk -F, '{print "\033[0;31m"$1"\033[0m" ": " $3 ", " $4}'|sort -n|uniq -c
+cat $processedfile |awk -F, ' {print "\033[0;31m"tolower($1)"\033[0m" ": " tolower($3) ", " tolower($4)}'|sort -n|uniq -c
 echo
 
 rm $processedfile
